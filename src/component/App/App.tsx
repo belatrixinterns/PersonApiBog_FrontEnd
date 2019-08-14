751b4d036776195d359efbe43854737670c0e6a5
jjimenezn-Belatrix <jjimenez@belatrixsf.com>
2019-08-14 14:54:02 -0500

    structure_header_footer

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Logo from '../../assets/images/logo-belatrix.png';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={Logo} className="App-logo" alt="logo" />
       </header>
+      <div className="Body">
+        
+      </div>
+      <div className="Footer">
+        
+      </div>
     </div>
   );
 }
