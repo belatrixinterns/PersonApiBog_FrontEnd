ca6c98d2b857bdfa08b5d6c0e97aaa5848f77c8e
jjimenezn-Belatrix <jjimenez@belatrixsf.com>
2019-08-20 13:03:14 -0500

    create_person_first_styles

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ListPersonPage from '../../container/ListPerson/ListPersonPage';
+import '../../sass/App.scss';
 import logo from '../../assets/images/logo-belatrix.png';
 import CreatePerson from '../../containers/CreatePerson/CreatePerson';
 
